f36880c6cffccd83dc81a7d44f0b97f6ef84121d
AntonioRodenasJurado <antonio.rodenas@insinno.es>
2025-03-18 13:58:26 +0100

    fix: ensure pool is initialized correctly in DatabaseService
    Signed-off-by: AntonioRodenasJurado <antonio.rodenas@insinno.es>

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,67 +1,67 @@
 import { Injectable } from '@angular/core';
-      import { Pool } from 'pg';
-      import { HttpClient } from '@angular/common/http';
-      import { environment } from '../../environments/environment';
+        import { Pool } from 'pg';
+        import { HttpClient } from '@angular/common/http';
+        import { environment } from '../../environments/environment';
 
-      interface Todo {
-        id: number;
-        name: string;
-        created_at: string;
-      }
-
-      @Injectable({
-        providedIn: 'root'
-      })
-      export class DatabaseService {
-        private dbPassword: string = '';
-        private pool: Pool;
-
-        constructor(private http: HttpClient) {
-          this.loadDbPassword();
+        interface Todo {
+          id: number;
+          name: string;
+          created_at: string;
         }
 
-        private loadDbPassword() {
-          this.http.get('https://your-secret-service.com/get-db-password')
-            .subscribe((response: any) => {
-              this.dbPassword = response.password;
-              this.initializePool();
-            });
-        }
+        @Injectable({
+          providedIn: 'root'
+        })
+        export class DatabaseService {
+          private dbPassword: string = '';
+          private pool!: Pool;
 
-        private initializePool() {
-          if (!environment.supabaseUrl) {
-            throw new Error('SUPABASE_URL is not defined');
+          constructor(private http: HttpClient) {
+            this.loadDbPassword();
           }
-          this.pool = new Pool({
-            user: 'postgres',
-            host: new URL(environment.supabaseUrl).hostname,
-            database: 'postgres',
-            password: this.dbPassword,
-            port: 5432,
-            ssl: {
-              rejectUnauthorized: false
+
+          private loadDbPassword() {
+            this.http.get('https://your-secret-service.com/get-db-password')
+              .subscribe((response: any) => {
+                this.dbPassword = response.password;
+                this.initializePool();
+              });
+          }
+
+          private initializePool() {
+            if (!environment.supabaseUrl) {
+              throw new Error('SUPABASE_URL is not defined');
             }
-          });
-        }
+            this.pool = new Pool({
+              user: 'postgres',
+              host: new URL(environment.supabaseUrl).hostname,
+              database: 'postgres',
+              password: this.dbPassword,
+              port: 5432,
+              ssl: {
+                rejectUnauthorized: false
+              }
+            });
+          }
 
-        async query(text: string, params?: any[]) {
-          const client = await this.pool.connect();
-          try {
-            const result = await client.query(text, params);
-            return result.rows;
-          } finally {
-            client.release();
+          async query(text: string, params?: any[]) {
+            const client = await this.pool.connect();
+            try {
+              const result = await client.query(text, params);
+              return result.rows;
+            } finally {
+              client.release();
+            }
+          }
+
+          async getTodos() {
+            return this.query('SELECT * FROM todos ORDER BY created_at DESC');
+          }
+
+          async addTodo(name: string) {
+            return this.query(
+              'INSERT INTO todos (name) VALUES ($1) RETURNING *',
+              [name]
+            );
           }
         }
-
-        async getTodos() {
-          return this.query('SELECT * FROM todos ORDER BY created_at DESC');
-        }
-
-        async addTodo(name: string) {
-          return this.query(
-            'INSERT INTO todos (name) VALUES ($1) RETURNING *',
-            [name]
-          );
-        }
-      }
